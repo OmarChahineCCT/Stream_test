090dd77a0dce5efc4459b4d78ea8e0608660f774
Omar Chahine <sbs25005@student.cct.ie>
2025-11-17 18:55:16 +0000

    Update uber.py

--- uber.py
+++ uber.py
@@ -26,5 +26,5 @@ hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24,range=(0,24))[0]
 st.bar_chart(hist_values)
 hour_to_filter=st.slider("hour",0,23,17)
 filtered_data=data[data[DATE_COLUMN].dt.hour==hour_to_filter]
-st.subheader(f"Map of Pickups{hours_to_filter}:00")
+st.subheader(f"Map of Pickups{hour_to_filter}:00")
 st.map(filtered_data)
