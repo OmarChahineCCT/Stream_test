9493840f97544cf00bd092e4f25b9f5517c08e1f
Omar Chahine <sbs25005@student.cct.ie>
2025-11-17 18:26:38 +0000

    Update uber.py

--- uber.py
+++ uber.py
@@ -10,7 +10,7 @@ DATE_COLUMN="date/time"
 def load_data(nrows):
 	data=pd.read_csv(DATA_URL,nrows=nrows)
 	lowercase=lambda x:str(x).lower()
-	data.rename(lowecase, axis="columns", inplace=True)
+	data.rename(lowercase, axis="columns", inplace=True)
 	data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
 	return data
 
