d3abfdfaa7a2d1493daec91c9c867763667c76d8
Omar Chahine <sbs25005@student.cct.ie>
2025-11-17 18:13:42 +0000

    Update uber.py

--- uber.py
+++ uber.py
@@ -3,3 +3,6 @@ import pandas as pd
 import numpy as np
 
 st.title("Uber Pickups in NY")
+
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/ streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATE_COLUMN="date/time"
