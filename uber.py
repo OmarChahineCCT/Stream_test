6a751455fed9e1d0ed725e1837eb7da2cb5833a4
Omar Chahine <sbs25005@student.cct.ie>
2025-11-17 18:49:41 +0000

    Update uber.py

--- uber.py
+++ uber.py
@@ -22,5 +22,5 @@ st.subheader("Raw Data")
 st.write(data)
 
 st.subheader("Number of pickups per hour")
-hist_values=np.histogram(data[DATE_COLUMN].dt.hour)
+hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24,range=(0,24))[0]
 st.bar_chart(hist_values)
